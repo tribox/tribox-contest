1e28231db57cf50a0fb0421da17f3d5be1ca0165
Kotaro Terada <kotaro@tribox.jp>
2016-05-02 02:02:30 +0900

    Remove comments

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -11,12 +11,6 @@ import play.api.mvc._
 @Singleton
 class HelloController @Inject() extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def hello = Action {
     Ok(views.html.hello("Hello world"))
   }
