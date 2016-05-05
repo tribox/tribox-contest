4484ab1c68b814e951bb62fa4852f4864907395f
Kotaro Terada <kotaro@tribox.jp>
2016-05-06 02:52:35 +0900

    Create contest page

---
+++ app/controllers/ContestController.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import javax.inject._
+import models._
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's contest pages.
+ */
+@Singleton
+class ContestController @Inject() extends Controller {
+
+  /**
+   * Contest pages
+   */
+  def contest(id: String) = Action {
+    val contestName = Play.application.configuration.getString("contest.name").getOrElse("tribox Contest")
+    val contestUrl = Play.application.configuration.getString("contest.url").getOrElse("https://contest.tribox.com/")
+    val firebaseappContest = Play.application.configuration.getString("firebaseapp.contest").getOrElse("tribox-contest")
+    Ok(views.html.contest(id, contestName, contestUrl, firebaseappContest))
+  }
+
+}
