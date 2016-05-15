beedd1519fea366bc931bd8a49a55cfbbf911e16
Kotaro Terada <kotaro@tribox.jp>
2016-05-15 21:33:38 +0900

    Create contestmanager

---
+++ contestmanager/create-season.js
@@ -0,0 +1,199 @@
+/**
+ * create-season.js
+ */
+
+var async = require('async');
+var request = require('request');
+
+var Config = require('./config.js');
+var Firebase = require('firebase');
+var contestRef = new Firebase('https://' + Config.CONTESTAPP + '.firebaseio.com/');
+
+// Create admin user
+var FirebaseTokenGenerator = require('firebase-token-generator');
+var tokenGenerator = new FirebaseTokenGenerator(Config.CONTESTAPP_SECRET);
+var token = tokenGenerator.createToken(
+    { uid: '1', some: 'arbitrary', data: 'here' },
+    { admin: true, debug: true }
+);
+
+// デフォルトの競技
+var EventsDefault = ['wca333', 'wca444', 'wca555', 'wca222', 'wca333bf', 'wca333oh', 'wca333fm'];
+
+
+var usage = function() {
+    console.error('Usage: node create-season.js 20161');
+    process.exit(1);
+}
+
+// イベントのデータを取得 (認証不要)
+contestRef.child('events').on('value', function(snap) {
+    var Events = snap.val();
+    //console.dir(Events);
+
+    // Create data
+    var year, season;
+    if (process.argv[2] === undefined || !( /^[0-9]{4}(1|2)$/.test(process.argv[2]) ) ) {
+        usage();
+    } else {
+        year = process.argv[2].substr(0, 4);
+        season = process.argv[2].substr(4);
+    }
+
+    var beginMonth, endMonth, seasonName;
+    if (season === '1') {
+        beginMonth = 0; // January
+        endMonth = 6;
+        seasonName = '前半期';
+    } else if (season === '2') {
+        beginMonth = 6; // July
+        endMonth = 0;
+        seasonName = '後半期';
+    } else {
+        process.exit(1);
+    }
+
+    // Unix timestamp for first contest
+    // 21:00:00 (JST) == 12:00:00 (UTC)
+    var beginDate = 1;
+    var beginTimestamp;
+    while (true) {
+        var d = new Date();
+        d.setUTCFullYear(year);
+        d.setUTCMonth(beginMonth);
+        d.setUTCDate(beginDate);
+        d.setUTCHours(12);
+        d.setUTCMinutes(0);
+        d.setUTCSeconds(0);
+        d.setUTCMilliseconds(0);
+        beginTimestamp = d.getTime();
+
+        // If sunday
+        if (d.getDay() === 0) {
+            break;
+        }
+        beginDate++;
+    }
+
+    // コンテストデータ
+    var contests = {};
+    var contestsIndexes = [];
+
+    // Basic contest data
+    for (var i = 1; ; i++) {
+        var d = new Date(beginTimestamp);
+        if (d.getUTCMonth() === endMonth) {
+            break;
+        }
+        var contestId = year + season + (('0') + i).substr(-2);
+        contestsIndexes.push('c' + contestId);
+
+        // next contest: Increment millisec for one week
+        var nextTimestamp = beginTimestamp + 7 * 24 * 60 * 60 * 1000;
+        contests['c' + contestId] = {
+            'contestId': parseInt(contestId),
+            'beginAt': beginTimestamp,
+            'endAt': nextTimestamp,
+            'contestName': 'tribox Contest ' + year + ' ' + seasonName + ' 第' + i + '回',
+            'year': parseInt(year),
+            'season': parseInt(season),
+            'number': i
+        }
+        contests['c' + contestId]['events'] = EventsDefault;
+
+        beginTimestamp = nextTimestamp;
+    }
+
+    // スクランブルを生成する
+    async.each(contestsIndexes, function(contestId, next) {
+        var contest = contests[contestId];
+
+        var url = 'http://localhost:2014/scramble/.json?seed=' + contestId + Config.SEED;
+        contest.events.forEach(function(eventId) {
+            url += '&' + eventId + '=' + Events[eventId].scramblePuzzle + '*' + Events[eventId].attempts;
+        });
+        //console.log(url);
+
+        var options = {
+            'url': url,
+            'method': 'GET',
+            'headers': { 'Content-Type': 'application/json' },
+            'json': true
+        };
+
+        // TNoodle でスクランブルを生成して取得
+        request(options, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                contests[contestId]['scrambles'] = {}
+                body.forEach(function(result) {
+                    var title = result.title;
+                    var scrambles = result.scrambles;
+                    contests[contestId]['scrambles'][title] = scrambles;
+                });
+                next();
+            } else {
+                console.error(error);
+                process.exit(1);
+            }
+        });
+
+    }, function(err) {
+        if (!err) {
+            //console.dir(contests);
+            console.log('Generated all scrambles');
+            saveToDB(contests, contestsIndexes);
+        } else {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+    //console.dir(contests);
+});
+
+// Save to DB
+var saveToDB = function(contests, contestsIndexes) {
+    // admin 権限でログインしてから操作する
+    contestRef.authWithCustomToken(token, function(error, authData) {
+        if (error) {
+            console.error('Authentication Failed!', error);
+        } else {
+            console.log('Authenticated successfully with payload:', authData);
+
+            // read current data
+            contestRef.child('contests').once('value', function(snap) {
+                var current = snap.val();
+
+                //console.dir(current);
+                //console.dir(contestsIndexes);
+                //console.dir(contests);
+
+                // write
+                async.each(contestsIndexes, function(contestId, next) {
+                    // 上書きはしない
+                    if (current[contestId] !== undefined) {
+                        console.error('Already exists:', contestId);
+                        next();
+                    } else {
+                        contestRef.child('contests').child(contestId).set(contests[contestId], function(error) {
+                            if (error) {
+                                console.error('Set failed');
+                                process.exit(1);
+                            } else {
+                                console.log('Set succeeded');
+                                next();
+                            }
+                        });
+                    }
+                }, function(err) {
+                    if (!err) {
+                        console.log('Completed');
+                        process.exit(0);
+                    } else {
+                        console.error(err);
+                        process.exit(1);
+                    }
+                });
+            });
+        }
+    });
+};
