1413e1865962e7940846d8fe91932180c5f876f7
Kotaro Terada <kotaro@tribox.jp>
2016-05-22 02:02:37 +0900

    Update front-end with SkyBlue CSS

--- app/controllers/SettingController.scala
+++ app/controllers/SettingController.scala
@@ -14,7 +14,7 @@ import play.api.Play.current
 class SettingController @Inject() extends Controller {
 
   /**
-   * Setting: Setting / First setting
+   * Setting: Setting / First setting / Other settings
    */
   def setting = Action {
     val contestName = Play.application.configuration.getString("contest.name").getOrElse("tribox Contest")
@@ -30,4 +30,17 @@ class SettingController @Inject() extends Controller {
     Ok(views.html.settingfirst(contestName, contestUrl, firebaseappContest))
   }
 
+  def settingemail = Action {
+    val contestName = Play.application.configuration.getString("contest.name").getOrElse("tribox Contest")
+    val contestUrl = Play.application.configuration.getString("contest.url").getOrElse("https://contest.tribox.com/")
+    val firebaseappContest = Play.application.configuration.getString("firebaseapp.contest").getOrElse("tribox-contest")
+    Ok(views.html.settingemail(contestName, contestUrl, firebaseappContest))
+  }
+
+  def settingpassword = Action {
+    val contestName = Play.application.configuration.getString("contest.name").getOrElse("tribox Contest")
+    val contestUrl = Play.application.configuration.getString("contest.url").getOrElse("https://contest.tribox.com/")
+    val firebaseappContest = Play.application.configuration.getString("firebaseapp.contest").getOrElse("tribox-contest")
+    Ok(views.html.settingpassword(contestName, contestUrl, firebaseappContest))
+  }
 }
