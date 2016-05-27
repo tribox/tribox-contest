1967cd3ac5c413825aa1c6fa63cbdc05522c3f3c
Kotaro Terada <kotaro@tribox.jp>
2016-05-27 15:10:35 +0900

    Update results data structure

--- contestmanager/create-season.js
+++ contestmanager/create-season.js
@@ -296,11 +296,15 @@ var saveResults = function(contests, contestsIndexes, callback) {
                         console.error('Already exists:', contestId);
                         next();
                     } else {
-                        contestRef.child('results').child(contestId).set({
-                            'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx': {
-                                '_dummy' : true
-                            }
-                        }, function(error) {
+                        var dummyResults = {};
+                        contests[contestId].events.forEach(function(e) {
+                            dummyResults[e] = {
+                                'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx': {
+                                    '_dummy' : true
+                                }
+                            };
+                        });
+                        contestRef.child('results').child(contestId).set(dummyResults, function(error) {
                             if (error) {
                                 console.error('Set failed');
                                 process.exit(1);
