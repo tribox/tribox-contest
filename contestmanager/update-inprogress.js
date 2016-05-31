0edbc983036a9085547094be477b48f0b95ca3aa
Kotaro Terada <kotaro@tribox.jp>
2016-05-31 20:22:48 +0900

    Creating result page

--- contestmanager/update-inprogress.js
+++ contestmanager/update-inprogress.js
@@ -55,6 +55,33 @@ contestRef.child('contests').once('value', function(snap) {
     // 現在時刻の開催コンテストが求まる
     console.log('cuurent contest:', founds[0]);
 
+    // 1個前のコンテストのタイムスタンプ
+    var lastTimestamp = currentTimestamp - (7 * 24 * 60 * 60 * 1000);
+    console.log('lastTimestamp:', lastTimestamp);
+
+    // 1個前のコンテストを見つける
+    var lastFounds = [];
+    Object.keys(Contests).forEach(function(contestId) {
+        var contest = Contests[contestId];
+        //console.log(contestId);
+        //console.dir(contest);
+
+        var beginAt = parseInt(contest['beginAt']);
+        var endAt = parseInt(contest['endAt']);
+        if (beginAt <= lastTimestamp && lastTimestamp <= endAt) {
+            lastFounds.push(contestId);
+        }
+    });
+
+    // 1個前のコンテストが見つからないか、複数見つかってしまう (ごくまれ)
+    if (lastFounds.length != 1) {
+        console.error('Try again');
+        process.exit(1);
+    }
+
+    // 現在時刻の開催コンテストが求まる
+    console.log('last contest:', lastFounds[0]);
+
     // 書き込む (admin権限で)
     contestRef.authWithCustomToken(token, function(error, authData) {
         if (error) {
@@ -64,7 +91,8 @@ contestRef.child('contests').once('value', function(snap) {
             console.log('Authentication successfully with payload:', authData);
 
             contestRef.child('inProgress').set({
-                'contest': founds[0]
+                'contest': founds[0],
+                'lastContest': lastFounds[0]
             }, function(error) {
                 if (error) {
                     console.err(err);
