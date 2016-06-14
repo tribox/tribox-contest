f448422abd61dc9c7b3c3670d29dc0e3316c77a5
Kotaro Terada <kotaro@tribox.jp>
2016-06-15 01:57:48 +0900

    Update collect

--- contestmanager/collect-results.js
+++ contestmanager/collect-results.js
@@ -35,11 +35,11 @@ argv.option([
         example: "'collect-results.js --contest=2016121' or 'collect-results.js -c 2016121'"
     },
     {
-        name: 'auto',
-        short: 'a',
+        name: 'lastcontest',
+        short: 'l',
         type: 'boolean',
         description: 'Set target contest to inProgress.lastContest',
-        example: "'collect-results.js --auto' or 'collect-results.js -a'"
+        example: "'collect-results.js --lastcontest' or 'collect-results.js -l'"
     },
     {
         name: 'tweet',
@@ -55,8 +55,11 @@ console.log(argvrun);
 // 対象コンテスト (c2016xxx)
 var targetContest;
 
+// 書き込むデータ
+var ready = {};
+
 // コンテストの集計結果を firebase と MySQL に書き込む
-var writeResults = function(ready) {
+var writeResults = function() {
     console.dir(ready);
 
     var connection = mysql.createConnection({
@@ -67,52 +70,127 @@ var writeResults = function(ready) {
     });
     connection.connect();
 
-    // 結果をひとつひとつ書き込む (順位とSPを上書きする)
+    // 書き込むデータを配列化する
+    var readyArr = [];
     Object.keys(ready).forEach(function(eventId) {
         Object.keys(ready[eventId]).forEach(function(userId) {
-            contestRef.child('results').child(targetContest).child(eventId).child(userId).update(ready[eventId][userId], function(error) {
-                if (error) {
-                    console.error(error);
-                } else {
-                    console.log('Completed updating place and SP: ' + eventId + ' ' + userId);
-                }
-            });
-
-            // ユーザの参加履歴を MySQL に保存する
-            // 存在する場合は update, 存在しない場合は insert
-            connection.query('SELECT id FROM competed_history WHERE user_id = ? AND contest_id = ? AND event_id = ?', [
-                userId, targetContest, eventId
-            ], function(error, results, fields) {
-                if (error) {
-                    console.error(error);
-                } else {
-                    if (results[0] && 'id' in results[0]) {
-                        console.log('UPDATE');
-                        connection.query('UPDATE competed_history SET has_competed = 1, updated_at = NOW() WHERE user_id = ? AND contest_id = ? AND event_id = ?', [
-                            userId, targetContest, eventId
-                        ], function(error, results, fields) {
-                            if (error) {
-                                console.error(error);
-                            }
-                        });
-                    } else {
-                        console.log('INSERT');
-                        connection.query('INSERT INTO competed_history SET ?', {
-                            'user_id': userId,
-                            'contest_id': targetContest,
-                            'event_id': eventId,
-                            'has_competed': 1
-                        }, function(error, results, fields) {
-                            if (error) {
-                                console.error(error);
-                            }
-                        });
-                    }
-                }
-            });
+            readyArr.push({ 'eventId': eventId, 'userId': userId });
         });
     });
 
+    // 結果をひとつひとつ書き込む (順位とSPを上書きする)
+    var count = 0;
+    async.each(readyArr, function(r, next) {
+        var eventId = r.eventId;
+        var userId = r.userId;
+
+        // Firebase に書き込む
+        contestRef.child('results').child(targetContest).child(eventId).child(userId).update(ready[eventId][userId], function(error) {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log('Completed updating place and SP: ' + eventId + ' ' + userId);
+
+                // ユーザの参加履歴を MySQL に保存する
+                // 存在する場合は update, 存在しない場合は insert
+                connection.query('SELECT id FROM competed_history WHERE user_id = ? AND contest_id = ? AND event_id = ?', [
+                    userId, targetContest, eventId
+                ], function(error, results, fields) {
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        if (results[0] && 'id' in results[0]) {
+                            console.log('UPDATE');
+                            connection.query('UPDATE competed_history SET has_competed = 1, updated_at = NOW() WHERE user_id = ? AND contest_id = ? AND event_id = ?', [
+                                userId, targetContest, eventId
+                            ], function(error, results, fields) {
+                                if (error) {
+                                    console.error(error);
+                                }
+                                count++;
+                                next();
+                            });
+                        } else {
+                            console.log('INSERT');
+                            connection.query('INSERT INTO competed_history SET ?', {
+                                'user_id': userId,
+                                'contest_id': targetContest,
+                                'event_id': eventId,
+                                'has_competed': 1
+                            }, function(error, results, fields) {
+                                if (error) {
+                                    console.error(error);
+                                }
+                                count++;
+                                next();
+                            });
+                        }
+                    }
+                });
+
+            }
+        });
+
+    }, function(err) {
+        if (!err) {
+            console.log('Completed all! updated ' + count + ' records');
+            process.exit(0);
+        } else {
+            console.error(err);
+            process.exit(1);
+        }
+    });
+
+};
+
+// 順位とSPの記録をリセットする
+var resetResults = function() {
+    // admin 権限でログインしてから操作する
+    contestRef.authWithCustomToken(token, function(error, authData) {
+        if (error) {
+            console.error('Authentication Failed!', error);
+        } else {
+            //console.log('Authenticated successfully with payload:', authData);
+
+            contestRef.child('results').child(targetContest).once('value', function(snapResults) {
+                var results = snapResults.val();
+
+                var resets = [];
+                Object.keys(results).forEach(function(eid) {
+                    Object.keys(results[eid]).forEach(function(uid) {
+                        if (!(results[eid][uid]._dummy)) {
+                            resets.push({ 'eid': eid, 'uid': uid });
+                        }
+                    });
+                });
+                //console.dir(resets);
+
+                // 実行
+                async.each(resets, function(r, next) {
+                    contestRef.child('results').child(targetContest).child(r.eid).child(r.uid).update({
+                        'place': null,
+                        'seasonPoint': null
+                    }, function(error) {
+                        if (error) {
+                            console.error(error);
+                            process.exit(1);
+                        } else {
+                            next();
+                        }
+                    });
+                }, function(err) {
+                    if (!err) {
+                        console.log('Completed reset!');
+                        writeResults();
+                    } else {
+                        console.error(err);
+                        process.exit(1);
+                    }
+                });
+
+            });
+        }
+    });
 };
 
 // コンテストの結果を集計（整形）する
@@ -129,11 +207,8 @@ var collectResults = function() {
             contestRef.child('results').child(targetContest).once('value', function(snapResults) {
                 var results = snapResults.val();
 
-                // 書き込むデータ
-                var ready = {};
-
+                ready = {};
                 Object.keys(results).forEach(function(eventId) {
-                    console.log(eventId);
                     ready[eventId] = {};
 
                     var place = 1;
@@ -171,7 +246,7 @@ var collectResults = function() {
 
                 });
 
-                writeResults(ready);
+                resetResults();
 
             });
         }
@@ -201,7 +276,6 @@ var checkFMC = function() {
                                 fmcResults[userId] = {};
 
                                 var solution = results[userId]['details'][0]['solution'];
-                                //console.log(solution);
 
                                 fmcchecker.checkSolution(scrambles['e333fm'][0], solution, function(moves) {
                                     console.log(scrambles['e333fm'][0]);
@@ -224,7 +298,6 @@ var checkFMC = function() {
                         }, function(err) {
                             if (!err) {
                                 //console.dir(fmcResults);
-
                                 async.each(Object.keys(fmcResults), function(userId, next) {
                                     contestRef.child('results').child(targetContest).child('e333fm').child(userId).child('result').update({
                                         'condition': fmcResults[userId]['result']['condition'],
@@ -265,6 +338,19 @@ var checkFMC = function() {
     });
 };
 
+// 存在するコンテストか調べる
+var checkExists = function() {
+    // コンテストから検索 (認証不要)
+    contestRef.child('contests').child(targetContest).once('value', function(snap) {
+        if (snap.exists()) {
+            checkFMC();
+        } else {
+            console.error('Contest does not exist');
+            process.exit(1);
+        }
+    });
+};
+
 // inProgess.lastContest を取得
 var getLastContest = function() {
     // lastContest を取得 (認証不要)
@@ -277,8 +363,8 @@ var getLastContest = function() {
 var main = function() {
     if (argvrun.options.contest) {
         targetContest = 'c' + argvrun.options.contest;
-        checkFMC();
-    } else if (argvrun.options.auto) {
+        checkExists();
+    } else if (argvrun.options.lastcontest) {
         getLastContest();
     }
 }
