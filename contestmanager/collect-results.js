ec1f3dbd15afb992d936fa9e017b00ca08830a3e
Kotaro Terada <kotaro@tribox.jp>
2016-06-20 23:42:30 +0900

    Update tweet text

--- contestmanager/collect-results.js
+++ contestmanager/collect-results.js
@@ -109,8 +109,8 @@ connection.connect();
 var doTweet = function() {
     if (argvrun.options.tweet) {
         // ツイートテキスト
-        // 例 "Yueh-Lin Tsai (tribox SCT) wins 今週のコンテスト [URL]"
-        var status = ' wins ' + targetContestObj.contestName
+        // 例 "Yueh-Lin Tsai (tribox SCT) wins Week 24 (2016, 1H) https://contest.tribox.com/contest/2016124"
+        var status = ' wins Week ' + targetContestObj.number + ' (' + targetContestObj.year + ', ' + targetContestObj.season + 'H)'
                    + ' https://contest.tribox.com/contest/' + targetContestObj.contestId;
         if (Users[winner].organization) {
             status = Users[winner].displayname + ' (' + Users[winner].organization + ')' + status;
