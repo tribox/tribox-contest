e74a8879e205d3e055577a2efafae0197e99566e
Kotaro Terada <kotaro@tribox.jp>
2018-12-08 23:07:57 +0900

    Create 'add skewb' script

---
+++ contestmanager/add-skewb.js
@@ -0,0 +1,68 @@
+/**
+ * add-skewb.js
+ *
+ * 2018-12 用
+ * イベントにピラミンクスを追加する。
+ */
+
+var Config = require('./config.js');
+var Firebase = require('firebase');
+var contestRef = new Firebase('https://' + Config.CONTESTAPP + '.firebaseio.com/');
+
+// Create admin user
+var FirebaseTokenGenerator = require('firebase-token-generator');
+var tokenGenerator = new FirebaseTokenGenerator(Config.CONTESTAPP_SECRET);
+var token = tokenGenerator.createToken(
+    { uid: '1', some: 'arbitrary', data: 'here' },
+    { admin: true, debug: true }
+);
+
+
+var updateEvents = function() {
+    // admin 権限でログインしてから操作する
+    contestRef.authWithCustomToken(token, function(error, authData) {
+        if (error) {
+            console.error('Authentication Failed!', error);
+        } else {
+            console.log('Authenticated successfully with payload:', authData);
+
+            // 2018-12 用
+            // スキューブ追加
+            contestRef.child('events').child('eskewb').set({
+                'attempts': 5,
+                'format': 'time',
+                'hasInspection': true,
+                'method': 'average',
+                'name': 'Skewb',
+                'scramblePuzzle': 'skewb'
+            }, function(error) {
+                if (error) {
+                    console.error(error);
+                    process.exit(1);
+                } else {
+                    contestRef.child('events').child('eskewb').setPriority(140, function(error) {
+                        if (error) {
+                            console.error(error);
+                            process.exit(1);
+                        } else {
+                            process.exit(0);
+                        }
+                    });
+                }
+            });
+
+        }
+    });
+}
+
+var main = function() {
+    // イベントのデータを取得 (認証不要)
+    contestRef.child('events').once('value', function(snap) {
+        var Events = snap.exportVal();
+        console.dir(Events);
+
+        updateEvents();
+    });
+};
+
+main();
