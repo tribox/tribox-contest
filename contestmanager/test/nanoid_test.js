b97e717c846e2c670424c66802d622e283e9f342
Kotaro Terada <kotaro@tribox.jp>
2022-03-04 11:54:11 +0000

    Update nanoid code

--- contestmanager/test/nanoid_test.js
+++ contestmanager/test/nanoid_test.js
@@ -3,11 +3,11 @@
  */
 
 var nanoid = require('nanoid');
-var generate = require('nanoid/generate');
+var nanoidCustom = nanoid.customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 21);
 
-var str1 = nanoid();
-var str2 = generate('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 21);
-var str3 = generate('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 21);
+var str1 = nanoid.nanoid();
+var str2 = nanoidCustom();
+var str3 = nanoidCustom();
 
 console.log(str1);
 console.log(str2);
