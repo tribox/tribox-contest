a3c1e88f8d80f97fab75b9441b60a8725e4756db
Kotaro Terada <kotaro@tribox.jp>
2024-04-03 19:45:57 +0900

    Fix miss variable names and descriptions

--- src/app.py
+++ src/app.py
@@ -104,6 +104,9 @@ def contest(cid):
         firebaseapp_contest=FIREBASEAPP_CONTEST,
         firebaseapp_contest_apikey=FIREBASEAPP_CONTEST_APIKEY,
         firebaseapp_contest_senderid=FIREBASEAPP_CONTEST_SENDERID,
+        firebaseapp_wca=FIREBASEAPP_WCA,
+        firebaseapp_wca_apikey=FIREBASEAPP_WCA_APIKEY,
+        firebaseapp_wca_senderid=FIREBASEAPP_WCA_SENDERID,
     )
 
 
