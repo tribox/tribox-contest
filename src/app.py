1a84b8e6cc5ead4d0bce723a83490410e2344ee3
mugi-tea <moriyuujp@gmail.com>
2025-02-09 02:54:50 +0900

    replace contest timer demo page

--- src/app.py
+++ src/app.py
@@ -296,7 +296,20 @@ def setting_unverify():
 ########################################
 @app.route("/demo/timer")
 def demo_timer():
-    raise NotImplementedError()
+    return render_template(
+        "contesttimerdemo.html",
+        id=id,
+        contest_description=CONTEST_DESCRIPTION,
+        contest_name=CONTEST_NAME,
+        contest_url=CONTEST_URL,
+        firebaseapp_contest=FIREBASEAPP_CONTEST,
+        firebaseapp_contest_apikey=FIREBASEAPP_CONTEST_APIKEY,
+        firebaseapp_contest_senderid=FIREBASEAPP_CONTEST_SENDERID,
+        firebaseapp_wca=FIREBASEAPP_WCA,
+        firebaseapp_wca_apikey=FIREBASEAPP_WCA_APIKEY,
+        firebaseapp_wca_senderid=FIREBASEAPP_WCA_SENDERID,
+    )
+
 
 
 ########################################
