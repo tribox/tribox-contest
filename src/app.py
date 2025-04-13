108541706b640364faeb1572becedcc343e40b4e
mugi-tea <moriyuujp@gmail.com>
2025-04-14 03:14:03 +0900

    Replase setting unverify

--- src/app.py
+++ src/app.py
@@ -269,7 +269,20 @@ def setting_verify():
 
 @app.route("/setting/unverify")
 def setting_unverify():
-    raise NotImplementedError()
+    return render_template(
+        "unverify.html",
+        contest_description=CONTEST_DESCRIPTION,
+        contest_name=CONTEST_NAME,
+        contest_url=CONTEST_URL,
+        firebaseapp_contest=FIREBASEAPP_CONTEST,
+        firebaseapp_contest_apikey=FIREBASEAPP_CONTEST_APIKEY,
+        firebaseapp_contest_senderid=FIREBASEAPP_CONTEST_SENDERID,
+        firebaseapp_wca=FIREBASEAPP_WCA,
+        firebaseapp_wca_apikey=FIREBASEAPP_WCA_APIKEY,
+        firebaseapp_wca_senderid=FIREBASEAPP_WCA_SENDERID,
+        userId="",
+        customerId=-1
+    )
 
 
 ########################################
